fe09ae6537eaef90bc2a04d5ff9bffc614501c01
Jose Jaime Come <josejaimecome@gmail.com>
2023-08-03 20:14:24 +0200

    Tags for add new field in form added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,61 @@
                @endforeach
             </div>
             <div style="margin-left: 300px;">
-               @if(isset($form_id)) {{ $form_id[0]->form_name }} @else No Form Selected @endif
+               @if(isset($form_id))
+                    <h3>{{ $form_id[0]->form_name }}</h3>
+                    <table style="width: 100%">
+                      <tr>
+                        <td>
+                            Name:
+                        </td>
+                        <td>
+                            <input type="name" class="form-control" id="name" placeholder="Enter name" style="width: 260px">
+                        </td>
+                        <td>
+                            Label:
+                        </td>
+                        <td>
+                            <input type="name" class="form-control" id="name" placeholder="Enter name" style="width: 260px">
+                        </td>
+                        <td>
+                            Type:
+                        </td>
+                        <td>
+                            <select class="form-control" id="qtype" style="width: 260px">
+                                <option>String</option>
+                                <option>Integer</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Type:
+                        </td>
+                        <td>
+                            <select class="form-control" id="qtype" style="width: 260px">
+                                <option>Type</option>
+                                <option>Select</option>
+                            </select>
+                        </td>
+                        <td>
+                            Length:
+                        </td>
+                        <td>
+                            <input type="name" class="form-control" id="name" placeholder="Enter name" style="width: 260px">
+                        </td>
+                        <td colspan="2">
+                            <form action="/formpage/{{ $f->id }}" style="float: right; padding-right: 4px;">
+                                <button type="submit" class="">Add</button>
+                            </form>
+                        </td>
+                      </tr>
+                      <tr>
+                      <td colspan="6"><hr style="width: 100%;"/></td>
+                      </tr>
+                    </table>
+               @else
+                    <h3>No Form Selected </h3>
+               @endif
             </div>
     </div>
 </main>
