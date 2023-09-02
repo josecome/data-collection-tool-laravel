6b7d4b794b48c06468e44cab71b4254adf43d1c9
Jose Jaime Come <josejaimecome@gmail.com>
2023-09-02 22:05:49 +0200

    Auth from Breeze added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormController;
 
@@ -9,8 +11,8 @@ use App\Http\Controllers\FormController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
@@ -22,3 +24,10 @@ Route::post('/deployform/', [FormController::class, 'deployform']);
 Route::post('/archiveform/', [FormController::class, 'archiveform']);
 Route::get('/deployed/{id}', [FormController::class, 'deployedFormOnlineGet']);
 Route::post('/deployedpost/{id}', [FormController::class, 'deployedFormOnlinePost']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
