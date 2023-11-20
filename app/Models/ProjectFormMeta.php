34aabdb93926b4211f36e6cc82550ed4903eb14c
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-20 20:24:21 +0200

    Models updated

--- app/Models/ProjectFormMeta.php
+++ app/Models/ProjectFormMeta.php
@@ -6,12 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProjectFormMeta extends Model
 {
     use HasFactory;
     protected $table = 'project_form_meta';
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function projectform(): HasMany
     {
