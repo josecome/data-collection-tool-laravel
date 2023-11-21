bb6647b33fdfe3060fd804e8e70d5b734e74179f
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-21 17:27:07 +0200

    Minor changes made

--- database/seeders/ProjectFormSeeder.php
+++ database/seeders/ProjectFormSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-'field_name'=>'LCD200',
 
 class ProjectFormSeeder extends Seeder
 {
